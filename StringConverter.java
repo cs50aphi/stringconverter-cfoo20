06654ce772df020e1e85f6de676ee32aa00c52bd
cfoo20 <cfoo20@punahou.edu>
2020-04-19 03:28:35 +0000

    Added method headers

---
+++ StringConverter.java
@@ -0,0 +1,20 @@
+// StringConverter by Cheyenne Foo
+public class StringConverter
+{
+    public static String reverse(String str)
+    {
+
+    }
+    public static boolean checkPalindrome(String str)
+    {
+
+    }
+    public static String pigLatinate(String str)
+    {
+
+    }
+    public static String shorthand(String str)
+    {
+
+    }
+}
