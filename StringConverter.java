74e0ff3ce090fec8ebacbdcae1f4237172afa633
cfoo20 <cfoo20@punahou.edu>
2020-04-19 04:03:16 +0000

    reverse completed

--- StringConverter.java
+++ StringConverter.java
@@ -3,26 +3,26 @@ public class StringConverter
 {
     public static String reverse(String str)
     {
-        reversed = "";
+        String reversed = "";
         // run through each char backwards
-        for (i = str.length() - 1; i >= 0; i--)
+        for (int i = str.length() - 1; i >= 0; i--)
         {
             // store each char in a reversed variable
-            reversed += str.get(i);
+            reversed += str.charAt(i);
         }
         // return reversed variable
         return reversed;
     }
     public static boolean checkPalindrome(String str)
     {
-
+        return false;
     }
     public static String pigLatinate(String str)
     {
-
+        return "";
     }
     public static String shorthand(String str)
     {
-
+        return "";
     }
 }
