daf79308f93ec5e0fb058bc6006530afc026c41f
cfoo20 <cfoo20@punahou.edu>
2020-04-20 08:21:06 +0000

    reverse private helper

--- StringConverter.java
+++ StringConverter.java
@@ -4,26 +4,28 @@ public class StringConverter
     // Reverse
     public static String reverse(String str)
     {
-        String reversed = "";
-        // run through each char backwards
-        for (int i = str.length() - 1; i >= 0; i--)
-        {
-            // store each char in a reversed variable
-            reversed += str.charAt(i);
-        }
-        // return reversed variable
-        return reversed;
+        return backwards(str);
     }
 
     // Palindrome
     public static boolean checkPalindrome(String str)
     {
         // remove all spaces and punctuation marks
+        str = str.replaceAll("\\p{Punct}","");
+        str = str.replaceAll("\\s", "");
         // convert all letters to lowercase
         // only check if even number of chars
+        if (str.length() % 2 == 1)
+        {
             // else return false
+            return false;
+        }
         // check if front half is the same as reversed back half
-            // return true if false
+        int half = str.length() / 2;
+        String front = str.substring(0, half);
+        String back = str.substring(half);
+
+            // return true if same
         // else return false
         return false;
     }
@@ -50,16 +52,29 @@ public class StringConverter
     // Shorthand
     public static String shorthand(String str)
     {
-        // replace all "and" and "And" with "&"
-        // replace all "to" and "To" with "2"
-        // replace all "you" and "You" with "U"
-        // replace all "for" and "For" with "4"
+        // Add space before and after to ensure only change words
+        // replace all " and " and " And " with "&"
+        // replace all " to " and " To " with "2"
+        // replace all " you " and " You " with "U"
+        // replace all " for " and " For " with "4"
         // iterate over each letter in word
-            // unless it was "you" or "You" before (campare old and new string)
+            // unless it was " you " or " You " before (campare old and new string)
             // remove all vowels
         // return new string
         return "";
     }
 
-    // backwards helper??
+    // backwards helper?? (just reverse)
+    private static String backwards(String str)
+    {
+        // run through each char backwards
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--)
+        {
+            // store each char in a reversed variable
+            reversed += str.charAt(i);
+        }
+        // return reversed variable
+        return reversed;
+    }
 }
