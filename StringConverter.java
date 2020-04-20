bd3245a8db64bbd45feb572753989f276979446e
cfoo20 <cfoo20@punahou.edu>
2020-04-20 05:02:11 +0000

    pseudocode for pig latin

--- StringConverter.java
+++ StringConverter.java
@@ -26,6 +26,19 @@ public class StringConverter
     }
     public static String pigLatinate(String str)
     {
+        String vowels = "aeiou";
+        // search through words for vowels
+        // iterate over each letter in word
+        // if first letter contains vowel
+            // return str + "yay"
+        // if contains any more vowels, get index of first vowel
+            // get start, which is first word until but not including first vowel
+            // get end, which is the rest of the word
+            // if capitalized
+                // capitalize end, uncapitalize start
+            // return end + start + "ay"
+        // else
+            // return str + "ay"
         return "";
     }
     public static String shorthand(String str)
